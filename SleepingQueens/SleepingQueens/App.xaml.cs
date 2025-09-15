9270e87d93c4b7d462989bd545095532d0981061
bendvir99 <bendvirnew@gmail.com>
2025-09-15 23:52:26 +0300

    AppIcon

---
+++ SleepingQueens/SleepingQueens/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SleepingQueens
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
